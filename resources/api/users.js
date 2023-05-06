9bd42f37befbe0d7d16417060c62de6c1fadcc8f
arfes0e2b3c <webyata@gmail.com>
2023-05-06 20:25:30 +0900

    third

--- resources/api/users.js
+++ resources/api/users.js
@@ -1,13 +1,8 @@
 exports.handler = async () => {
   return {
-    statusCode: 200,
+    statusCode: 401,
     body: {
-      message: "User details by user_id",
-      user: {
-        user_id: "TaroYamada",
-        nickname: "たろー",
-        comment: "僕は元気です",
-      },
+      message: "Authentication Failed",
     },
   };
 };
